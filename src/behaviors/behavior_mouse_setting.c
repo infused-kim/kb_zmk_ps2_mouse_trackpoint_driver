9de296b0f4bb27e5ec8e791b3940c66024fe01ba
Kim Streich <kim@infusedinsight.com>
2023-03-29 20:51:18 -0600

    PS2 Mouse: Removed queue from mouse setting behavior PS2 Mouse: PS2 Mouse: A queue was added to the ps2 gpio driver itself. So there is no need for it here anymore.

--- src/behaviors/behavior_mouse_setting.c
+++ src/behaviors/behavior_mouse_setting.c
@@ -12,29 +12,19 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 // #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
 
-K_THREAD_STACK_DEFINE(bg_work_stack_area, 1024);
-static struct k_work_q bg_work_q;
-struct k_work bg_worker;
-
 static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
                                      struct zmk_behavior_binding_event event)
 {
-    // switch (binding->param1) {
-    //     case MS_SAMPLING_RATE_INCR:
-    //         return zmk_ps2_set_sampling_rate_incr();
-    //     case MS_SAMPLING_RATE_DECR:
-    //         return zmk_ps2_set_sampling_rate_decr();
-    // }
-    k_work_submit_to_queue(&bg_work_q, &bg_worker);
+    switch (binding->param1) {
+        case MS_SAMPLING_RATE_INCR:
+            return zmk_ps2_set_sampling_rate_incr();
+        case MS_SAMPLING_RATE_DECR:
+            return zmk_ps2_set_sampling_rate_decr();
+    }
 
     return -ENOTSUP;
 }
 
-static void zmk_mouse_ps2_setting_bg_worker(struct k_work *work)
-{
-    LOG_INF("On worker queue zmk_ps2_set_sampling_rate_decr...");
-    zmk_ps2_set_sampling_rate_decr();
-}
 static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
                                       struct zmk_behavior_binding_event event)
 {
@@ -43,15 +33,6 @@ static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
 
 // Initialization Function
 static int zmk_behavior_mouse_setting_init(const struct device *dev) {
-    k_work_queue_start(
-        &bg_work_q,
-        bg_work_stack_area,
-        K_THREAD_STACK_SIZEOF(bg_work_stack_area),
-        50,
-        NULL
-    );
-    k_work_init(&bg_worker, zmk_mouse_ps2_setting_bg_worker);
-
     return 0;
 };
 
