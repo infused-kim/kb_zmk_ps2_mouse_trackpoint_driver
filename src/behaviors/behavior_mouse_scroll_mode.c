2af519c0e8eed8ac967c2d67bfb49e3f364699bf
Kim Streich <kim@infusedinsight.com>
2024-01-08 17:30:29 -0600

    Mouse Settings & Scroll Mode: Apply new auto-formatting

--- src/behaviors/behavior_mouse_scroll_mode.c
+++ src/behaviors/behavior_mouse_scroll_mode.c
@@ -9,36 +9,25 @@
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
-                                     struct zmk_behavior_binding_event event)
-{
+                                     struct zmk_behavior_binding_event event) {
     zmk_mouse_ps2_scroll_mode_enable();
 
     return ZMK_BEHAVIOR_OPAQUE;
 }
 
 static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
-                                      struct zmk_behavior_binding_event event)
-{
+                                      struct zmk_behavior_binding_event event) {
     zmk_mouse_ps2_scroll_mode_disable();
 
     return ZMK_BEHAVIOR_OPAQUE;
 }
 
 // Initialization Function
-static int zmk_behavior_mouse_scroll_mode_init(const struct device *dev) {
-    return 0;
-};
+static int zmk_behavior_mouse_scroll_mode_init(const struct device *dev) { return 0; };
 
-static const struct behavior_driver_api
-zmk_behavior_mouse_scroll_mode_driver_api = {
-    .binding_pressed = on_keymap_binding_pressed,
-    .binding_released = on_keymap_binding_released
-};
+static const struct behavior_driver_api zmk_behavior_mouse_scroll_mode_driver_api = {
+    .binding_pressed = on_keymap_binding_pressed, .binding_released = on_keymap_binding_released};
 
-DEVICE_DT_INST_DEFINE(
-    0,
-    zmk_behavior_mouse_scroll_mode_init, NULL,
-    NULL, NULL,
-    APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
-    &zmk_behavior_mouse_scroll_mode_driver_api
-);
+DEVICE_DT_INST_DEFINE(0, zmk_behavior_mouse_scroll_mode_init, NULL, NULL, NULL, APPLICATION,
+                      CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
+                      &zmk_behavior_mouse_scroll_mode_driver_api);
--- src/behaviors/behavior_mouse_setting.c
+++ src/behaviors/behavior_mouse_setting.c
@@ -16,71 +16,43 @@
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
-                                     struct zmk_behavior_binding_event event)
-{
+                                     struct zmk_behavior_binding_event event) {
     switch (binding->param1) {
 
-        case MS_TP_SENSITIVITY_INCR:
-            return zmk_mouse_ps2_tp_sensitivity_change(
-                INCREMENT_TP_SENSITIVITY
-            );
-        case MS_TP_SENSITIVITY_DECR:
-            return zmk_mouse_ps2_tp_sensitivity_change(
-                -INCREMENT_TP_SENSITIVITY
-            );
+    case MS_TP_SENSITIVITY_INCR:
+        return zmk_mouse_ps2_tp_sensitivity_change(INCREMENT_TP_SENSITIVITY);
+    case MS_TP_SENSITIVITY_DECR:
+        return zmk_mouse_ps2_tp_sensitivity_change(-INCREMENT_TP_SENSITIVITY);
 
-        case MS_TP_NEG_INERTIA_INCR:
-            return zmk_mouse_ps2_tp_neg_inertia_change(
-                INCREMENT_TP_NEG_INERTIA
-            );
-        case MS_TP_NEG_INERTIA_DECR:
-            return zmk_mouse_ps2_tp_neg_inertia_change(
-                -INCREMENT_TP_NEG_INERTIA
-            );
+    case MS_TP_NEG_INERTIA_INCR:
+        return zmk_mouse_ps2_tp_neg_inertia_change(INCREMENT_TP_NEG_INERTIA);
+    case MS_TP_NEG_INERTIA_DECR:
+        return zmk_mouse_ps2_tp_neg_inertia_change(-INCREMENT_TP_NEG_INERTIA);
 
-        case MS_TP_VALUE6_INCR:
-            return zmk_mouse_ps2_tp_value6_upper_plateau_speed_change(
-                INCREMENT_TP_VALUE6
-            );
-        case MS_TP_VALUE6_DECR:
-            return zmk_mouse_ps2_tp_value6_upper_plateau_speed_change(
-                -INCREMENT_TP_VALUE6
-            );
+    case MS_TP_VALUE6_INCR:
+        return zmk_mouse_ps2_tp_value6_upper_plateau_speed_change(INCREMENT_TP_VALUE6);
+    case MS_TP_VALUE6_DECR:
+        return zmk_mouse_ps2_tp_value6_upper_plateau_speed_change(-INCREMENT_TP_VALUE6);
 
-        case MS_TP_PTS_THRESHOLD_INCR:
-            return zmk_mouse_ps2_tp_pts_threshold_change(
-                INCREMENT_TP_PTS_THRESHOLD
-            );
-        case MS_TP_PTS_THRESHOLD_DECR:
-            return zmk_mouse_ps2_tp_pts_threshold_change(
-                -INCREMENT_TP_PTS_THRESHOLD
-            );
+    case MS_TP_PTS_THRESHOLD_INCR:
+        return zmk_mouse_ps2_tp_pts_threshold_change(INCREMENT_TP_PTS_THRESHOLD);
+    case MS_TP_PTS_THRESHOLD_DECR:
+        return zmk_mouse_ps2_tp_pts_threshold_change(-INCREMENT_TP_PTS_THRESHOLD);
     }
 
     return -ENOTSUP;
 }
 
 static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
-                                      struct zmk_behavior_binding_event event)
-{
+                                      struct zmk_behavior_binding_event event) {
     return ZMK_BEHAVIOR_OPAQUE;
 }
 
 // Initialization Function
-static int zmk_behavior_mouse_setting_init(const struct device *dev) {
-    return 0;
-};
+static int zmk_behavior_mouse_setting_init(const struct device *dev) { return 0; };
 
-static const struct behavior_driver_api
-zmk_behavior_mouse_setting_driver_api = {
-    .binding_pressed = on_keymap_binding_pressed,
-    .binding_released = on_keymap_binding_released
-};
+static const struct behavior_driver_api zmk_behavior_mouse_setting_driver_api = {
+    .binding_pressed = on_keymap_binding_pressed, .binding_released = on_keymap_binding_released};
 
-DEVICE_DT_INST_DEFINE(
-    0,
-    zmk_behavior_mouse_setting_init, NULL,
-    NULL, NULL,
-    APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
-    &zmk_behavior_mouse_setting_driver_api
-);
+DEVICE_DT_INST_DEFINE(0, zmk_behavior_mouse_setting_init, NULL, NULL, NULL, APPLICATION,
+                      CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &zmk_behavior_mouse_setting_driver_api);
