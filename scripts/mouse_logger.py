26d54c98349914df83f98d7eea759c704419131e
Kim Streich <kim@infusedinsight.com>
2024-04-25 22:03:05 -0600

    Mouse Logger: Log median time whenver the mouse stops moving

--- scripts/mouse_logger.py
+++ scripts/mouse_logger.py
@@ -1,33 +1,71 @@
 #!/usr/bin/env python3
+"""
+Script that logs mouse movements to debug mouse performance issues.
+
+Requirements:
+    - pip install pynput
+"""
 
 from pynput import mouse
 import time
+import threading
+import statistics
 
-# Define global variables to store the previous mouse position
+# Global variables to calculate mouse movement
 prev_x, prev_y = 0, 0
 prev_time = time.time()
 
-# Define a callback function to handle mouse movement events
+# Variables for summary task
+time_intervals = []
+lock = threading.Lock()
+delayed_task_thread = None
+prev_summary_interval = None
+
+
+def summary_task():
+    with lock:
+        if time_intervals:
+            median_interval = statistics.median(time_intervals)
+            print(
+                f'Median time between movements: {median_interval * 1000:.2f} '
+                f'ms\n\n\n'
+            )
+            time_intervals.clear()
+
+
+def schedule_summary_task(delay_ms):
+    global delayed_task_thread
+    if delayed_task_thread is not None and delayed_task_thread.is_alive():
+        delayed_task_thread.cancel()
+
+    delayed_task_thread = threading.Timer(delay_ms / 1000, summary_task)
+    delayed_task_thread.start()
+
+
 def on_move(x, y):
     global prev_x, prev_y, prev_time
 
-    # Calculate the distance moved
     distance_x = x - prev_x
     distance_y = y - prev_y
 
-    # Get the current time
     current_time = time.time()
-
-    # Calculate the time interval of movement
     time_interval = current_time - prev_time
 
-    # Print the movement distance and time interval
-    print('Mouse moved distance - X: {0}, Y: {1}, time interval: {2}'.format(distance_x, distance_y, time_interval))
+    with lock:
+        time_intervals.append(time_interval)
 
-    # Update the previous position and time
+    print(
+        f'Mouse moved distance - '
+        f'X: {distance_x:+7.2f}, '
+        f'Y: {distance_y:+7.2f}, '
+        f'time interval: {time_interval * 1000:5.2f} ms'
+    )
+
     prev_x, prev_y = x, y
     prev_time = current_time
 
-# Create a mouse listener
+    schedule_summary_task(500)
+
+
 with mouse.Listener(on_move=on_move) as listener:
     listener.join()
